667f1ead657dd9ca495202a73f4d00fa03f83bb5
roryheart <almoneef.meera@gmail.com>
2025-09-09 18:15:47 +0300

    Less or Greater than 15

--- Conditional.py
+++ Conditional.py
@@ -1,12 +1,20 @@
-x = int( input ("Enter a number:"))
-if x>0: 
-    print ("This number is positive.")
-else:
-     print ("This number is negative.")
+# x = int( input ("Enter a number:"))
+# if x>0: 
+#     print ("This number is positive.")
+# else:
+#      print ("This number is negative.")
 
-x = int( input ("How much did you buy it for?"))
-y= int( input ("How much are you selling it for?"))
-if x>y: 
-    print ("This number is a loss.")
+# x = int( input ("How much did you buy it for?"))
+# y= int( input ("How much are you selling it for?"))
+# if x>y: 
+#     print ("This number is a loss.")
+# else:
+#      print ("This is a profit.")
+
+n = int( input ("What is your number?"))
+if n>15:
+    print ("Number is greater than 15")
 else:
-     print ("This is a profit.")
+    print("Number is less than 15")
+
+ 
